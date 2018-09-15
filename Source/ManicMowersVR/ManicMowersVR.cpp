77b95a8226512b52971372153bdb66c12ffb41b2
Justin <Hamltoe@gmail.com>
2018-09-15 16:15:30 -0400

    Initial commit

---
+++ Source/ManicMowersVR/ManicMowersVR.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "ManicMowersVR.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, ManicMowersVR, "ManicMowersVR" );
